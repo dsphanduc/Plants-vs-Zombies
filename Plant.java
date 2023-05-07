e65f01e7ddfcac152e3fd7de9672b4414255d7da
dsphanduc <phanduc.ds@gmail.com>
2023-05-07 23:38:05 +0700

    finish class Plant

---
+++ Plant.java
@@ -0,0 +1,18 @@
+public class Plant {
+    public int health = 200;
+
+    public int x;
+    public int y;
+
+    public GamePanel gp;
+
+
+    public Plant(GamePanel parent,int x,int y){
+        this.x = x;
+        this.y = y;
+        gp = parent;
+    }
+
+    public void stop(){}
+    
+}
